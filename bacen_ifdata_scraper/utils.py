79c3d30437ff908e2a49916720a7106dab664874
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2023-12-29 21:13:24 -0300

    refactor: remove ensure_clickable() from utils.py

--- bacen_ifdata_scraper/utils.py
+++ bacen_ifdata_scraper/utils.py
@@ -18,12 +18,10 @@
 #  ------------------------------------------------------------------------------
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 
-def initialize_webdriver() -> webdriver:
+def initialize_webdriver() -> WebDriver:
     """
     Initializes a WebDriver session with Firefox.
 
@@ -35,39 +33,3 @@ def initialize_webdriver() -> webdriver:
     driver = webdriver.Firefox()
 
     return driver
-
-
-def ensure_clickable(driver: webdriver, wait_time: int, by_method: str, locator: str):
-    """
-    Waits for an element to be clickable on a web page and then clicks it.
-
-    This function uses explicit wait to pause the execution until the specified
-    element is determined to be clickable. Once clickable, the function
-    performs a click action on the element.
-
-    Parameters:
-    - driver (webdriver): The WebDriver instance being used to interact with the web page.
-    - wait_time (int): The maximum number of seconds to wait for the element to become clickable.
-    - by_method (str): The method used to locate the element (e.g., By.ID, By.XPATH).
-    - locator (str): The locator string used with the by_method to find the element.
-
-    Raises:
-    - TimeoutException: If the element is not clickable within the specified wait_time.
-
-    Returns:
-    None
-    """
-
-    try:
-        element = WebDriverWait(driver, wait_time).until(
-            EC.element_to_be_clickable((by_method, locator))
-        )
-        element.click()
-    except TimeoutException:
-        print(f"Timeout: O elemento {
-              locator} não se tornou clicável após {wait_time} segundos.")
-        raise
-    except NoSuchElementException:
-        print(f"Não encontrado: O elemento {
-              locator} não foi encontrado na página.")
-        raise
