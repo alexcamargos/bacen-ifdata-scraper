37c0f2f8e654b2c9c7d81a0d419e524c952f5fb9
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2024-01-06 10:24:15 -0300

    refactor: add context manager (with) for WebDriver

--- run.py
+++ run.py
@@ -37,46 +37,50 @@ from bacen_ifdata_scraper.utils import (initialize_webdriver,
 from bacen_ifdata_scraper.storage.processing import process_downloaded_files
 from bacen_ifdata_scraper.institutions_type import InstitutionType as INSTITUTION
 from bacen_ifdata_scraper.reports_type import REPORTS
+from bacen_ifdata_scraper.exceptions import IfDataScraperException
 
 
 if __name__ == '__main__':
-    driver = initialize_webdriver()
+    try:
+        # Initialize the WebDriver session.
+        with initialize_webdriver() as driver:
+            session = Session(driver, CONFIG.URL)
+            session.open()
 
-    session = Session(driver, CONFIG.URL)
-    session.open()
+            # Ensure that the download directory exists.
+            ensure_directory(build_directory_path(CONFIG.DOWNLOAD_DIRECTORY))
 
-    # Ensure that the download directory exists.
-    ensure_directory(build_directory_path(CONFIG.DOWNLOAD_DIRECTORY))
+            data_base = session.get_data_bases()
 
-    data_base = session.get_data_bases()
+            session.download_reports(data_base[0],
+                                     INSTITUTION.FINANCIAL_CONGLOMERATES,
+                                     REPORTS[INSTITUTION.FINANCIAL_CONGLOMERATES].PORTFOLIO_NUMBER_CLIENTS_OPERATIONS
+                                     )
 
-    session.download_reports(data_base[0],
-                             INSTITUTION.FINANCIAL_CONGLOMERATES,
-                             REPORTS[INSTITUTION.FINANCIAL_CONGLOMERATES].PORTFOLIO_NUMBER_CLIENTS_OPERATIONS
-                             )
+            institution_directory = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
+                                                         INSTITUTION.FINANCIAL_CONGLOMERATES.name.lower())
+            report_directory = build_directory_path(institution_directory,
+                                                    REPORTS[INSTITUTION.FINANCIAL_CONGLOMERATES].
+                                                    PORTFOLIO_NUMBER_CLIENTS_OPERATIONS.name.lower())
+            ensure_directory(institution_directory)
+            ensure_directory(report_directory)
 
-    institution_directory = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
-                                                 INSTITUTION.FINANCIAL_CONGLOMERATES.name.lower())
-    report_directory = build_directory_path(institution_directory,
-                                            REPORTS[INSTITUTION.FINANCIAL_CONGLOMERATES].
-                                            PORTFOLIO_NUMBER_CLIENTS_OPERATIONS.name.lower())
-    ensure_directory(institution_directory)
-    ensure_directory(report_directory)
+            # Wait for the download to finish before processing the file.
+            if wait_for_download_completion(CONFIG.DOWNLOAD_DIRECTORY,
+                                            CONFIG.DOWNLOAD_FILE_NAME):
+                report_file_path = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
+                                                        INSTITUTION.FINANCIAL_CONGLOMERATES.name.lower(),
+                                                        REPORTS[INSTITUTION.FINANCIAL_CONGLOMERATES].
+                                                        PORTFOLIO_NUMBER_CLIENTS_OPERATIONS.name.lower(),
+                                                        f'{data_base[0].replace('/', '_')}.csv')
 
-    # Wait for the download to finish before processing the file.
-    if wait_for_download_completion(CONFIG.DOWNLOAD_DIRECTORY,
-                                    CONFIG.DOWNLOAD_FILE_NAME):
-        report_file_path = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
-                                                INSTITUTION.FINANCIAL_CONGLOMERATES.name.lower(),
-                                                REPORTS[INSTITUTION.FINANCIAL_CONGLOMERATES].
-                                                PORTFOLIO_NUMBER_CLIENTS_OPERATIONS.name.lower(),
-                                                f'{data_base[0].replace('/', '_')}.csv')
-
-        process_downloaded_files(build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
-                                                      'CONFIG.DOWNLOAD_FILE_NAME'),
-                                 report_file_path)
-    else:
-        print('Download was not completed in the expected time.')
-
-    # Clean up the session, closing the browser and show report.
-    session.cleanup()
+                process_downloaded_files(build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
+                                                              'CONFIG.DOWNLOAD_FILE_NAME'),
+                                         report_file_path)
+            else:
+                print('Download was not completed in the expected time.')
+    except IfDataScraperException as error:
+        print(error.message)
+    finally:
+        # Clean up the session, closing the browser and show report.
+        session.cleanup()
