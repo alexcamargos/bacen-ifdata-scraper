ac219e559ccfd23d517ac9feb1f58c3056db08af
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2024-01-06 10:06:05 -0300

    refactor: Sanitize the imports

--- run.py
+++ run.py
@@ -30,12 +30,12 @@ License: MIT
 
 import bacen_ifdata_scraper.config as CONFIG
 from bacen_ifdata_scraper.session import Session
-from bacen_ifdata_scraper.utils import initialize_webdriver
-from bacen_ifdata_scraper.utils import build_directory_path
-from bacen_ifdata_scraper.utils import ensure_directory
-from bacen_ifdata_scraper.utils import wait_for_download_completion
+from bacen_ifdata_scraper.utils import (initialize_webdriver,
+                                        build_directory_path,
+                                        ensure_directory,
+                                        wait_for_download_completion)
 from bacen_ifdata_scraper.storage.processing import process_downloaded_files
-from bacen_ifdata_scraper.institutions_type import InstitutionType as Institution
+from bacen_ifdata_scraper.institutions_type import InstitutionType as INSTITUTION
 from bacen_ifdata_scraper.reports_type import REPORTS
 
 
@@ -51,13 +51,14 @@ if __name__ == '__main__':
     data_base = session.get_data_bases()
 
     session.download_reports(data_base[0],
-                             Institution.FINANCIAL_CONGLOMERATES,
-                             REPORTS[Institution.FINANCIAL_CONGLOMERATES].PORTFOLIO_NUMBER_CLIENTS_OPERATIONS)
+                             INSTITUTION.FINANCIAL_CONGLOMERATES,
+                             REPORTS[INSTITUTION.FINANCIAL_CONGLOMERATES].PORTFOLIO_NUMBER_CLIENTS_OPERATIONS
+                             )
 
     institution_directory = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
-                                                 Institution.FINANCIAL_CONGLOMERATES.name.lower())
+                                                 INSTITUTION.FINANCIAL_CONGLOMERATES.name.lower())
     report_directory = build_directory_path(institution_directory,
-                                            REPORTS[Institution.FINANCIAL_CONGLOMERATES].
+                                            REPORTS[INSTITUTION.FINANCIAL_CONGLOMERATES].
                                             PORTFOLIO_NUMBER_CLIENTS_OPERATIONS.name.lower())
     ensure_directory(institution_directory)
     ensure_directory(report_directory)
@@ -66,8 +67,8 @@ if __name__ == '__main__':
     if wait_for_download_completion(CONFIG.DOWNLOAD_DIRECTORY,
                                     CONFIG.DOWNLOAD_FILE_NAME):
         report_file_path = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
-                                                Institution.FINANCIAL_CONGLOMERATES.name.lower(),
-                                                REPORTS[Institution.FINANCIAL_CONGLOMERATES].
+                                                INSTITUTION.FINANCIAL_CONGLOMERATES.name.lower(),
+                                                REPORTS[INSTITUTION.FINANCIAL_CONGLOMERATES].
                                                 PORTFOLIO_NUMBER_CLIENTS_OPERATIONS.name.lower(),
                                                 f'{data_base[0].replace('/', '_')}.csv')
 
