f7337bf47fc0f3f37bca4ff050a6a04d4470c26d
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2024-01-06 09:45:01 -0300

    feat: Move download files to proper folder

--- run.py
+++ run.py
@@ -31,7 +31,8 @@ License: MIT
 import bacen_ifdata_scraper.config as CONFIG
 from bacen_ifdata_scraper.session import Session
 from bacen_ifdata_scraper.utils import initialize_webdriver
-from bacen_ifdata_scraper.utils import ensure_download_directory
+from bacen_ifdata_scraper.utils import build_directory_path
+from bacen_ifdata_scraper.utils import ensure_directory
 from bacen_ifdata_scraper.utils import wait_for_download_completion
 from bacen_ifdata_scraper.storage.processing import process_downloaded_files
 from bacen_ifdata_scraper.institutions_type import InstitutionType as Institution
@@ -45,20 +46,34 @@ if __name__ == '__main__':
     session.open()
 
     # Ensure that the download directory exists.
-    ensure_download_directory(CONFIG.DOWNLOAD_DIRECTORY)
+    ensure_directory(build_directory_path(CONFIG.DOWNLOAD_DIRECTORY))
 
     data_base = session.get_data_bases()
 
     session.download_reports(data_base[0],
                              Institution.FINANCIAL_CONGLOMERATES,
-                             REPORTS[Institution.FINANCIAL_CONGLOMERATES]
-                             .PORTFOLIO_NUMBER_CLIENTS_OPERATIONS,
-                             )
+                             REPORTS[Institution.FINANCIAL_CONGLOMERATES].PORTFOLIO_NUMBER_CLIENTS_OPERATIONS)
+
+    institution_directory = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
+                                                 Institution.FINANCIAL_CONGLOMERATES.name.lower())
+    report_directory = build_directory_path(institution_directory,
+                                            REPORTS[Institution.FINANCIAL_CONGLOMERATES].
+                                            PORTFOLIO_NUMBER_CLIENTS_OPERATIONS.name.lower())
+    ensure_directory(institution_directory)
+    ensure_directory(report_directory)
 
     # Wait for the download to finish before processing the file.
-    if wait_for_download_completion(CONFIG.DOWNLOAD_DIRECTORY, 'dados.csv'):
-        process_downloaded_files(
-            'dados.csv', f'{data_base[0].replace('/', '_')}.csv')
+    if wait_for_download_completion(CONFIG.DOWNLOAD_DIRECTORY,
+                                    CONFIG.DOWNLOAD_FILE_NAME):
+        report_file_path = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
+                                                Institution.FINANCIAL_CONGLOMERATES.name.lower(),
+                                                REPORTS[Institution.FINANCIAL_CONGLOMERATES].
+                                                PORTFOLIO_NUMBER_CLIENTS_OPERATIONS.name.lower(),
+                                                f'{data_base[0].replace('/', '_')}.csv')
+
+        process_downloaded_files(build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
+                                                      'CONFIG.DOWNLOAD_FILE_NAME'),
+                                 report_file_path)
     else:
         print('Download was not completed in the expected time.')
 
