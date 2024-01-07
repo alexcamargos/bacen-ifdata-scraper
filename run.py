64ea4d682350f5983990b64e03334f6b3e775272
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2024-01-07 10:30:37 -0300

    feat: The main scraper function applies all current logic in the system.

--- run.py
+++ run.py
@@ -33,9 +33,10 @@ from bacen_ifdata_scraper.session import Session
 from bacen_ifdata_scraper.utils import (initialize_webdriver,
                                         build_directory_path,
                                         ensure_directory,
-                                        wait_for_download_completion)
+                                        wait_for_download_completion,
+                                        check_file_already_downloaded)
 from bacen_ifdata_scraper.storage.processing import process_downloaded_files
-from bacen_ifdata_scraper.institutions_type import InstitutionType as INSTITUTION
+from bacen_ifdata_scraper.institutions_type import InstitutionType as INSTITUTIONS
 from bacen_ifdata_scraper.reports_type import REPORTS
 from bacen_ifdata_scraper.exceptions import IfDataScraperException
 
@@ -47,37 +48,37 @@ def main_scraper(_session: Session, _data_base: str, _institution, _report):
     ensure_directory(build_directory_path(CONFIG.DOWNLOAD_DIRECTORY))
 
     # Create the directory for the institution and report.
-    institution_directory = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY, _institution.name.lower())
+    institution_directory = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
+                                                 _institution.name.lower())
     ensure_directory(institution_directory)
     report_directory = build_directory_path(institution_directory, _report.name.lower())
     ensure_directory(report_directory)
 
-    # Download the reports.
-    _session.download_reports(_data_base, _institution, _report)
+    report_file_path = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
+                                            _institution.name.lower(),
+                                            _report.name.lower(),
+                                            f'{_data_base.replace('/', '_')}.csv')
 
-    # Wait for the download to finish before processing the file.
-    if wait_for_download_completion(CONFIG.DOWNLOAD_DIRECTORY,
-                                    CONFIG.DOWNLOAD_FILE_NAME):
-        report_file_path = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
-                                                _institution.name.lower(),
-                                                _report.name.lower(),
-                                                f'{_data_base.replace('/', '_')}.csv')
-
-        process_downloaded_files(build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
-                                                      CONFIG.DOWNLOAD_FILE_NAME
-                                                      ),
-                                 report_file_path)
+    # Check if the file was already downloaded.
+    if check_file_already_downloaded(report_file_path):
+        print(f'Report "{_report.name}" from "{_institution.name}" referring to "{
+            _data_base}" was already downloaded, skipping...')
     else:
-        print('Download was not completed in the expected time.')
+        # Download the reports.
+        _session.download_reports(_data_base, _institution, _report)
+
+        # Wait for the download to finish before processing the file.
+        if wait_for_download_completion(CONFIG.DOWNLOAD_DIRECTORY,
+                                        CONFIG.DOWNLOAD_FILE_NAME):
+            process_downloaded_files(build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
+                                                          CONFIG.DOWNLOAD_FILE_NAME),
+                                     report_file_path)
+        else:
+            print('Download was not completed in the expected time.')
 
 
 if __name__ == '__main__':
     session = None
-
-    # Institution and report to be downloaded.
-    institution = INSTITUTION.FINANCIAL_CONGLOMERATES
-    report = REPORTS[institution].PORTFOLIO_NUMBER_CLIENTS_OPERATIONS
-
     try:
         # Initialize the WebDriver session.
         with initialize_webdriver() as driver:
@@ -87,8 +88,11 @@ if __name__ == '__main__':
             # Get the available data bases.
             data_base = session.get_data_bases()
 
-            # Download the reports.
-            main_scraper(session, data_base[0], institution, report)
+            for institution in INSTITUTIONS:
+                for report in REPORTS[institution]:
+                    for data in data_base:
+                        # Download the reports.
+                        main_scraper(session, data, institution, report)
     except IfDataScraperException as error:
         print(error.message)
     finally:
