f68899ec9b290a52db37699dfa74bf48729b6b02
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2024-01-28 10:59:31 -0300

    refactor: Normalizes a CSV file from Bacen If.Data
    Correcting the normalization process that was not being
    applied in all necessary situations.

--- bacen_ifdata/data_manager/processing.py
+++ bacen_ifdata/data_manager/processing.py
@@ -38,7 +38,37 @@ def check_file_already_processed(output_directory: Path, file: str) -> bool:
 
 
 def normalize_csv(institution: StrEnum, report: StrEnum, file: str) -> bool:
-    """Normalizes a CSV file by removing the first line and the last two lines."""
+    """
+    Normalizes a CSV file from Bacen If.Data by correcting its header
+    structure and removing inconsistent lines at both the start and end
+    of the file.
+
+    The normalization process involves several steps:
+    1. Correcting header inconsistencies due to non-standard CSV formatting by Bacen,
+       where column titles might appear in different lines based on header groupings.
+    2. Removing any empty final column in the header.
+    3. Discarding additional lines at the end of the file that contain consolidated
+       report information and do not conform to the standard data format.
+
+    The function checks if the file has already been normalized before proceeding.
+
+    Args:
+        institution (StrEnum): An enumerated value representing the institution.
+        report (StrEnum): An enumerated value representing the report type.
+        file (str): The name of the file to be normalized.
+
+    Returns:
+        bool: True if the file is successfully normalized, False otherwise.
+
+    Raises:
+        FileNotFoundError: If the specified file is not found in the input directory.
+        IOError: If an input/output error occurs during file processing.
+
+    Note:
+        The function assumes the presence of a specific directory structure
+        for input (downloaded CSV files) and output (normalized CSV files),
+        as defined in the configuration module (`CONFIG`).
+    """
 
     # Diretório onde os arquivos CSV baixados são armazenados.
     input_path = build_directory_path(CONFIG.DOWNLOAD_DIRECTORY,
@@ -61,33 +91,63 @@ def normalize_csv(institution: StrEnum, report: StrEnum, file: str) -> bool:
                 open(f'{output_path}\\{file}', 'w', encoding='utf-8') as output_file:
             data = input_file.readlines()
 
-            # Os arquivos CSV do Bacen às vezes têm uma última coluna
-            # vazia no cabeçalho. Para garantir a consistência dos dados,
-            # essa coluna sem conteúdo será identificada e removida.
+            # The developers responsible for the Bacen website deviated from
+            # the standard CSV format. They implemented an unusual approach
+            # by including groups of headers within the CSV file.
+            # This results in an inconsistent structure where the column
+            # title may appear in either the first or third line, varying
+            # according to the number of groupings in the headers.
+            # This peculiarity presents a unique challenge in handling these files.
             header = data[0].split(';')
-            if header[-1] == '\n':
-                # Removendo a última coluna do cabeçalho.
-                header.pop()
+            start = 1
 
-            # Para facilitar a análise dos dados, a primeira linha do arquivo,
-            # que contem o cabeçalho, será removida.
-            data.pop(0)
-
-            # Os arquivos CSV do Bacen contêm linhas extras no final com informações
-            # consolidadas sobre o relatório. Estas linhas não seguem o formato
-            # padrão e precisam ser removidas para uma análise correta dos dados.
-            # A estratégia é identificar e descartar todas as linhas a partir da primeira
-            # que não corresponde ao tamanho do cabeçalho.
-            for index, line in enumerate(data):
-                if len(line.rstrip().split(';')) != len(header):
-                    data = data[:index]
-                    # Encontramos a primeira linha que não corresponde ao tamanho do cabeçalho.
-                    # Portanto, podemos o loop.
+            for index in range(1, 6):
+                current_line = data[index].split(';')
+                if current_line[0] == '':
+                    start += 1
+                else:
                     break
 
-            # Salvando o conteúdo no arquivo normalizado.
+            if start > 1:
+                for column_index, column_value in enumerate(header):
+                    if column_value == '':
+                        for row_index in range(start - 1, 0, -1):
+                            cols = data[row_index].split(';')
+                            if len(cols) > column_index and cols[column_index].strip():
+                                header[column_index] = cols[column_index].strip()
+                                break
+
+            # On certain occasions, the CSV files provided by Bacen
+            # feature a final column with no data in the header.
+            # To ensure the integrity and consistency of the data,
+            # this empty column will be identified and removed.
+            if header[-1] in ('\n', ''):
+                # Remove the last column.
+                header.pop()
+
+            # Remove the inconsistent header.
+            data = data[start:]
+
+            # The CSV files provided by Bacen include additional lines
+            # at the end containing consolidated report information.
+            # These extra lines do not conform to the standard format
+            # of the rest of the file and thus need to be removed for
+            # an accurate data analysis. The approach involves identifying
+            # and discarding all lines following the first one that does
+            # not have the same number of columns as the header.
+            mismatch_index = next(
+                (index for index, line in enumerate(data)
+                 if len(line.rstrip().split(';')) != len(header)), len(data)
+            )
+            data = data[:mismatch_index]
+
+            # Save the normalized file.
             output_file.writelines(data)
+
+        # Return True if the file was successfully normalized.
         return True
+
+    # Handle exceptions.
     except FileNotFoundError as error:
         print(f'File not found: {error.filename}')
         return False
