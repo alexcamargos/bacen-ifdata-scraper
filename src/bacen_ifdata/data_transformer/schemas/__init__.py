e8a03db916b28b1897d3e32cc2016148d6c425a7
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2026-02-07 14:14:17 -0300

    feat(schemas): add FinancialConglomeratePortfolioLegalPersonTypeMaturitySchema and update imports

--- src/bacen_ifdata/data_transformer/schemas/__init__.py
+++ src/bacen_ifdata/data_transformer/schemas/__init__.py
@@ -38,6 +38,9 @@ from bacen_ifdata.data_transformer.schemas.financial_conglomerates.liabilities i
 from bacen_ifdata.data_transformer.schemas.financial_conglomerates.portfolio_individuals_type_maturity import (
     FinancialConglomeratePortfolioIndividualsTypeMaturitySchema,
 )
+from bacen_ifdata.data_transformer.schemas.financial_conglomerates.portfolio_legal_person_type_maturity import (
+    FinancialConglomeratePortfolioLegalPersonTypeMaturitySchema,
+)
 from bacen_ifdata.data_transformer.schemas.financial_conglomerates.summary import FinancialConglomerateSummarySchema
 from bacen_ifdata.data_transformer.schemas.individual_institutions.assets import IndividualInstitutionAssetsSchema
 from bacen_ifdata.data_transformer.schemas.individual_institutions.income_statement import (
@@ -120,6 +123,9 @@ FINANCIAL_CONGLOMERATE_INCOME_STATEMENT_SCHEMA = FinancialConglomerateIncomeStat
 FINANCIAL_CONGLOMERATE_PORTFOLIO_INDIVIDUALS_TYPE_MATURITY_SCHEMA = (
     FinancialConglomeratePortfolioIndividualsTypeMaturitySchema()
 )
+FINANCIAL_CONGLOMERATE_PORTFOLIO_LEGAL_PERSON_TYPE_MATURITY_SCHEMA = (
+    FinancialConglomeratePortfolioLegalPersonTypeMaturitySchema()
+)
 
 # Individual Institutions schemas
 INDIVIDUAL_INSTITUTION_SUMMARY_SCHEMA = IndividualInstitutionSummarySchema()
@@ -150,6 +156,7 @@ __all__ = [
     'FINANCIAL_CONGLOMERATE_LIABILITIES_SCHEMA',
     'FINANCIAL_CONGLOMERATE_INCOME_STATEMENT_SCHEMA',
     'FINANCIAL_CONGLOMERATE_PORTFOLIO_INDIVIDUALS_TYPE_MATURITY_SCHEMA',
+    'FINANCIAL_CONGLOMERATE_PORTFOLIO_LEGAL_PERSON_TYPE_MATURITY_SCHEMA',
     # Individual Institutions
     'INDIVIDUAL_INSTITUTION_SUMMARY_SCHEMA',
     'INDIVIDUAL_INSTITUTION_ASSETS_SCHEMA',
