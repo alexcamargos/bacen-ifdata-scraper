eaf970e69572f8094145cf8332b06e42b8d63f6e
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2026-02-10 20:58:17 -0300

    refactor(mocks): update MockWebDriver class documentation and improve code readability

--- tests/mocks/web_driver.py
+++ tests/mocks/web_driver.py
@@ -1,22 +1,6 @@
-#!/usr/bin/env python
-# encoding: utf-8
-#
-#  ------------------------------------------------------------------------------
-#  Name: web_driver.py
-#  Version: 0.0.1
-#  Summary: Banco Central do Brasil IF.data Scraper
-#           Este sistema foi projetado para automatizar o download dos
-#           relatórios da ferramenta IF.data do Banco Central do Brasil.
-#           Criado para facilitar a integração com ferramentas automatizadas de
-#           análise e visualização de dados, garantido acesso fácil e oportuno
-#           aos dados.
-#
-#  Author: Alexsander Lopes Camargos
-#  Author-email: alcamargos@vivaldi.net
-#
-#  License: MIT
-#  ------------------------------------------------------------------------------
+"""Mocks for WebDriver interactions in tests."""
 
+from typing import Optional
 
 from selenium.common.exceptions import NoSuchElementException
 
@@ -25,46 +9,38 @@ from .element import DelayedVisibilityMockElement, MockElement
 
 class MockWebDriver:
     """
-    Essa classe provê uma maneira simplificada de simular a interação com um navegador web
-    sem a necessidade de um ambiente de navegador real. É especialmente útil em testes
-    unitários onde a interação com elementos da web é necessária.
+    This class provides a simplified way to simulate interaction with a web browser
+    without the need for a real browser environment. It is especially useful in unit tests
+    where interaction with web elements is required.
 
-    A classe utiliza MockElement para simular elementos da web que podem ser
-    'clicáveis', 'não clicáveis' ou 'inexistentes' para testar diferentes cenários
-    de interação do usuário e comportamento do navegador.
-
-    Atributos:
-        elements (dict): Um dicionário mapeando identificadores de elementos
-                         para instâncias de MockElement ou None.
-
-    Métodos:
-        find_element(by, value): Simula a busca de um elemento no navegador.
-                                 Retorna um MockElement ou None.
+    The class uses MockElement to simulate web elements that can be
+    'clickable', 'unclickable', or 'nonexistent' to test different user interaction scenarios
+    and browser behavior.
     """
 
     def __init__(self):
-        """Inicializa a classe MockWebDriver."""
+        """Initializes the MockWebDriver class."""
 
-        self.elements = {
+        self.elements: dict[str, Optional[MockElement]] = {
             'clickable': MockElement(is_displayed=True, is_enabled=True),
             'unclickable': MockElement(is_displayed=True, is_enabled=False),
             'delayed': DelayedVisibilityMockElement(is_displayed=False, is_enabled=False, delay=2),
             'nonexistent': None,
         }
 
-    def find_element(self, by, value):
-        """Simula a busca de um elemento no navegador."""
+    def find_element(self, by: str, value: str) -> MockElement:
+        """Simulates searching for an element in the browser."""
 
         element = self.elements.get(value)
         if element is None:
-            raise NoSuchElementException(f"Elemento com {by}='{value}' não foi encontrado")
+            raise NoSuchElementException(f"Element with {by}='{value}' was not found")
 
         return element
 
-    def execute_script(self, script, element):
-        """Simula a execução de um script JavaScript.]
+    def execute_script(self, script: str, element: MockElement) -> None:
+        """Simulates executing a JavaScript script.
 
-        No caso de um script de clique, chama o método click() do elemento.
+        In the case of a click script, it calls the element's click() method.
         """
 
         if 'click' in script.lower():
