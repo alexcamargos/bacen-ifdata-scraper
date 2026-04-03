dcc6e9c6d3e7695eb081b9cf8bd235923c9cc89a
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2026-04-03 09:54:53 -0300

    refactor(sql): specify selected columns in institution dimension for improved clarity and performance

--- src/bacen_ifdata/data_analytics/models/gold/view_instituicoes_historico_nomes.sql
+++ src/bacen_ifdata/data_analytics/models/gold/view_instituicoes_historico_nomes.sql
@@ -4,25 +4,28 @@ WITH fatos AS (
     SELECT id_instituicao, id_data, nome_instituicao_historico FROM {{ ref('fato_resumo_financeiro') }}
 ),
 dim_inst AS (
-    SELECT * FROM {{ ref('dim_instituicao') }}
+    SELECT
+        id_instituicao,
+        codigo_origem,
+        nome_instituicao,
+        tipo_instituicao,
+        nome_conglomerado,
+        tipo_relatorio_bcb,
+        segmento,
+        cidade,
+        uf,
+        regiao,
+        tipo_de_controle,
+        tcb,
+        consolidado_bancario,
+        classe_instituicao
+    FROM {{ ref('int_instituicao_enriquecida') }}
 ),
 dim_tempo AS (
-    SELECT * FROM {{ ref('dim_tempo') }}
-),
-dim_seg AS (
-    SELECT * FROM {{ ref('dim_segmento') }}
-),
-dim_loc AS (
-    SELECT * FROM {{ ref('dim_localizacao') }}
-),
-dim_cont AS (
-    SELECT * FROM {{ ref('dim_controle') }}
-),
-dim_cons AS (
-    SELECT * FROM {{ ref('dim_consolidado') }}
-),
-dim_classe AS (
-    SELECT * FROM {{ ref('dim_classe_instituicao') }}
+    SELECT
+        id_data,
+        data
+    FROM {{ ref('dim_tempo') }}
 ),
 
 base_historico AS (
@@ -32,7 +35,7 @@ base_historico AS (
         
         -- Instituição
         i.codigo_origem,
-        i.nome as nome_instituicao,
+        i.nome_instituicao,
         f.nome_instituicao_historico,
         i.tipo_instituicao,
         i.nome_conglomerado,
@@ -41,14 +44,14 @@ base_historico AS (
         i.tipo_relatorio_bcb,
         
         -- Atributos Dimensionais
-        s.descricao as segmento,
-        l.cidade,
-        l.uf,
-        l.regiao,
-        c.descricao as tipo_de_controle,
-        cb.codigo as tcb,
-        cb.descricao as consolidado_bancario,
-        cl.descricao as classe_instituicao,
+        i.segmento,
+        i.cidade,
+        i.uf,
+        i.regiao,
+        i.tipo_de_controle,
+        i.tcb,
+        i.consolidado_bancario,
+        i.classe_instituicao,
         
         -- Data
         t.data as data_base,
@@ -62,11 +65,6 @@ base_historico AS (
     FROM fatos f
     LEFT JOIN dim_inst i ON f.id_instituicao = i.id_instituicao
     LEFT JOIN dim_tempo t ON f.id_data = t.id_data
-    LEFT JOIN dim_seg s ON i.id_segmento = s.id_segmento
-    LEFT JOIN dim_loc l ON i.id_localizacao = l.id_localizacao
-    LEFT JOIN dim_cont c ON i.id_controle = c.id_controle
-    LEFT JOIN dim_cons cb ON i.id_consolidado = cb.id_consolidado
-    LEFT JOIN dim_classe cl ON i.id_classe = cl.id_classe
 )
 
 SELECT
