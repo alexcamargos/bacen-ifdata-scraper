31d0cc9101bed796050f37f107b443d6e668e81f
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2026-04-03 09:16:11 -0300

    feat(sql): update institution dimension selection in financial report views

--- src/bacen_ifdata/data_analytics/models/gold/view_resumo_financeiro_relatorios.sql
+++ src/bacen_ifdata/data_analytics/models/gold/view_resumo_financeiro_relatorios.sql
@@ -4,22 +4,25 @@ WITH fatos AS (
     SELECT * FROM {{ ref('fato_resumo_financeiro') }}
 ),
 dim_inst AS (
-    SELECT * FROM {{ ref('dim_instituicao') }}
+    SELECT
+        id_instituicao,
+        codigo_origem,
+        nome_instituicao,
+        tipo_instituicao,
+        nome_conglomerado,
+        tipo_relatorio_bcb,
+        segmento,
+        segmento_descricao,
+        cidade,
+        uf,
+        regiao,
+        tipo_de_controle,
+        tcb,
+        consolidado_bancario
+    FROM {{ ref('int_instituicao_enriquecida') }}
 ),
 dim_tempo AS (
     SELECT * FROM {{ ref('dim_tempo') }}
-),
-dim_seg AS (
-    SELECT * FROM {{ ref('dim_segmento') }}
-),
-dim_loc AS (
-    SELECT * FROM {{ ref('dim_localizacao') }}
-),
-dim_cont AS (
-    SELECT * FROM {{ ref('dim_controle') }}
-),
-dim_cons AS (
-    SELECT * FROM {{ ref('dim_consolidado') }}
 )
 
 SELECT
@@ -28,7 +31,7 @@ SELECT
     
     -- Instituição
     i.codigo_origem,
-    i.nome as nome_instituicao,
+    i.nome_instituicao,
     f.nome_instituicao_historico,
     i.tipo_instituicao,
     i.nome_conglomerado,
@@ -37,13 +40,14 @@ SELECT
     i.tipo_relatorio_bcb,
     
     -- Atributos Dimensionais
-    s.descricao as segmento,
-    l.cidade,
-    l.uf,
-    l.regiao,
-    c.descricao as tipo_de_controle,
-    cb.codigo as tcb,
-    cb.descricao as consolidado_bancario,
+    i.segmento,
+    i.segmento_descricao,
+    i.cidade,
+    i.uf,
+    i.regiao,
+    i.tipo_de_controle,
+    i.tcb,
+    i.consolidado_bancario,
     
     -- Data
     t.data as data_base,
@@ -63,7 +67,3 @@ SELECT
 FROM fatos f
 LEFT JOIN dim_inst i ON f.id_instituicao = i.id_instituicao
 LEFT JOIN dim_tempo t ON f.id_data = t.id_data
-LEFT JOIN dim_seg s ON i.id_segmento = s.id_segmento
-LEFT JOIN dim_loc l ON i.id_localizacao = l.id_localizacao
-LEFT JOIN dim_cont c ON i.id_controle = c.id_controle
-LEFT JOIN dim_cons cb ON i.id_consolidado = cb.id_consolidado
